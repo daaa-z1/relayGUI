a581dabe8bd2bd835dbed9cb61be6289028b9cab
daaa-z1 <113659254+daaa-z1@users.noreply.github.com>
2023-05-31 15:08:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -54,29 +54,29 @@ def delete_pin(odd_pin, even_pin):
 def setup_pins():
     pins = get_pins()
     for pin in pins:
-        wp.pinMode(pin[0], wp.OUTPUT)
-        wp.digitalWrite(pin[0], wp.HIGH)
-        wp.pinMode(pin[1], wp.OUTPUT)
-        wp.digitalWrite(pin[1], wp.HIGH)
+        wp.pinMode(pin[0], 1)
+        wp.digitalWrite(pin[0], 1)
+        wp.pinMode(pin[1], 1)
+        wp.digitalWrite(pin[1], 1)
 
 # Function to turn on an odd pin
 def turn_on_odd_pin(odd_pin):
-    wp.digitalWrite(odd_pin, wp.LOW)
+    wp.digitalWrite(odd_pin, 0)
     update_pin_state(odd_pin, None, 0)
 
 # Function to turn off an odd pin
 def turn_off_odd_pin(odd_pin):
-    wp.digitalWrite(odd_pin, wp.HIGH)
+    wp.digitalWrite(odd_pin, 1)
     update_pin_state(odd_pin, None, 1)
 
 # Function to turn on an even pin
 def turn_on_even_pin(even_pin):
-    wp.digitalWrite(even_pin, wp.LOW)
+    wp.digitalWrite(even_pin, 0)
     update_pin_state(None, even_pin, 0)
 
 # Function to turn off an even pin
 def turn_off_even_pin(even_pin):
-    wp.digitalWrite(even_pin, wp.HIGH)
+    wp.digitalWrite(even_pin, 1)
     update_pin_state(None, even_pin, 1)
 
 # Route for the home page
@@ -85,47 +85,46 @@ def home():
     pins = get_pins()
     return render_template('main.html', pins=pins)
 
-@app.route('/toggle_pin/<int:pin_number>', methods=['POST'])
-def toggle_pin(pin_number):
+@app.route('/toggle_pin/<int:pin_number>/<int:even_number>', methods=['POST'])
+def toggle_pin(pin_number, even_number):
     state = int(request.form['state'])
     if pin_number % 2 == 0:
         # Control even pin
         if state == 1:
-            turn_on_even_pin(pin_number)
+            turn_on_even_pin(even_number)
         elif state == 0:
-            turn_off_even_pin(pin_number)
+            turn_off_even_pin(even_number)
     else:
         # Control odd pin
         if state == 1:
             turn_on_odd_pin(pin_number)
         elif state == 0:
             turn_off_odd_pin(pin_number)
+
     return redirect("/")
 
-# Route for stop button
-@app.route('/stop_pin/<int:odd_pin>/<int:even_pin>', methods=['POST'])
-def stop_pin_route(odd_pin, even_pin):
-    turn_off_odd_pin(odd_pin)
-    turn_off_even_pin(even_pin)
-    return redirect("/")
-
-# Route for adding a pin
 @app.route('/add_pin', methods=['POST'])
 def add_pin_route():
     odd_pin = int(request.form['odd_pin'])
     even_pin = int(request.form['even_pin'])
     name = request.form['name']
     add_pin(odd_pin, even_pin, name)
-    setup_pins()  # Set up the newly added pin
     return redirect("/")
 
-# Route for deleting a pin
 @app.route('/delete_pin/<int:odd_pin>/<int:even_pin>', methods=['POST'])
 def delete_pin_route(odd_pin, even_pin):
     delete_pin(odd_pin, even_pin)
     return redirect("/")
 
+@app.route('/stop_pin/<int:odd_pin>/<int:even_pin>', methods=['POST'])
+def stop_pin_route(odd_pin, even_pin):
+    if odd_pin:
+        turn_off_odd_pin(odd_pin)
+    if even_pin:
+        turn_off_even_pin(even_pin)
+    return redirect("/")
+
 if __name__ == '__main__':
     create_table()
     setup_pins()
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run()
